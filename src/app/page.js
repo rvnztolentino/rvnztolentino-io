e9eba0cbf2671fcc4a2af558bf1e55238d03d5c0
Renz <johnrenztolentino1028@gmail.com>
2024-12-26 20:01:15 +0800

    vercel: redeploy

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,17 @@
 import Image from 'next/image';
-import Link from 'next/link';
 
 export default function Home() {
   return (
     <>
       <main className="flex flex-col justify-center min-h-screen space-y-4 xs:w-4/5 lg:w-2/5 mx-auto xs:px-0 lg:px-0 2xl:px-20 font-noto-sans text-left">
-        <Image src="/cube.svg" width={80} height={80} alt="svg" className='mt-20' />
+        <Image src="/cube.svg" width={80} height={80} alt="svg" className="mt-20" />
         <h1 className="text-4xl text-dark-gray font-playfair-display mt-20">renz</h1>
         <p className="text-md text-dark-gray mt-2">
-          hey there! thanks for visiting my website. i'm currently working on perfecting it
+          hey there! thanks for visiting my website. im currently working on perfecting it
           and deciding what content to showcase here. stay tuned for updates!
         </p>
         <p className="text-md text-light-gray mt-2 pb-10">Nothing yet! Check back later :)</p>
-        <Image src="/neko.gif" width={500} height={500} alt="gif" className='pb-20'/>
+        <Image src="/neko.gif" width={500} height={500} alt="gif" className="pb-20" />
       </main>
     </>
   );
